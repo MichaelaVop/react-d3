2647c03bb5c180e01f9ae7ea0fc68050c1185838
MichaelaVop <70598230+MichaelaVop@users.noreply.github.com>
2021-03-10 21:12:06 +0100

    modul - stateless

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = ({cityName, temp}) => {
+    return (
+        <div className="row">
+            <h2>{cityName}</h2>     
+            <h5>Today's Temperature: {temp}</h5>       
+        </div>
+    )
+}
+
+export default Header
+
